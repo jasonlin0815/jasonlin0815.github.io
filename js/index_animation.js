da7d3b27744b434a869a43fbd2c1d607acbe1eea
Jason Lin <jasonlin0815@gmail.com>
2017-02-12 01:06:54 -0800

    index.html draft v1 finished

--- js/index_animation.js
+++ js/index_animation.js
@@ -7,3 +7,18 @@ $(document).ready(function()
         .animate({ opacity: 1 }, 1200, 'swing');
     }, 3500);
 });
+
+$('a').click(function(event)
+{
+    // Delay propagation to next page
+    event.preventDefault();
+    var dst = this.getAttribute("href");
+
+    // Play short entering animation
+
+    // Delay time
+    setTimeout(function()
+    {
+        window.location = dst;
+    });
+});
