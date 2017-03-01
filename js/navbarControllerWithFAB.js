c1cd73d8ba0ed905d21bdaa3f8921d713cb5f4e0
Jason Lin <jasonlin0815@gmail.com>
2017-03-01 00:36:34 -0800

    fab & navbar animation, about page

--- js/navbarControllerWithFAB.js
+++ js/navbarControllerWithFAB.js
@@ -1,22 +1,39 @@
 $(document).ready(function()
 {
-    $("#fab").on("click", fabOpenNav);
-    $("#navbar-close-button").on("click", fabCloseNav);
+    var navMoveValue = parseFloat($("#navbar-wrapper").css("height"));
+
+    // Animate FAB onclick opening navbar
+    $("#fab").on("click", function() {
+        // Animate fab dissapearing
+        $(this).animate({
+            opacity: 0
+        }, 500, function() {
+            $(this).css({display: "none"});
+        });
+
+        // Animate nav bar dropping down
+        $("#navbar-wrapper").animate({
+            marginTop: 0
+        }, 1000, "easeOutBounce");
+        $("body").animate({
+            marginTop: navMoveValue
+        }, 1000, "easeOutBounce");
+    });
+
+    // Animate navbar onclick closing and return fab
+    $("#navbar-close-button").on("click", function() {
+        // Appearing fab
+        $("#fab").css({display: "flex"});
+        $("#fab").animate({
+            opacity: 1
+        }, 500);
+
+        // Closing nav bar
+        $("#navbar-wrapper").animate({
+            marginTop: -navMoveValue
+        }, 1000, "easeOutExpo");
+        $("body").animate({
+            marginTop: 0
+        }, 1000, "easeOutExpo");
+    });
 });
-
-function fabOpenNav()
-{
-    $(this).animate({opacity: 0}, 100);
-
-    var navbarHeight = $("#navbar-wrapper").css("height");
-    $("#navbar").css("display", "block");
-    $("body").css("margin-top", navbarHeight);
-}
-
-function fabCloseNav()
-{
-    $("#fab").animate({opacity: 1}, 100);
-
-    $("#navbar").css("display", "none");
-    $("body").css("margin-top", 0);
-}
