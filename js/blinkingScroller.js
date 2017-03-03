139069f31061202747383b49cd2f19bc3ccc96e0
Jason Lin <jasonlin0815@gmail.com>
2017-03-03 00:45:19 -0800

    dropdown footer effect

--- js/blinkingScroller.js
+++ js/blinkingScroller.js
@@ -1,8 +1,22 @@
 $(document).ready(function()
 {
-    $("#blinking-scroller-1").animate({
-        width: 15,
-        height: 15,
-        opacity: 0.1
-    }, 1100);
+    var t = 0;
+    $("#blinking-scroller").children().each(function() {
+        var element = $(this);
+        setTimeout(
+            setInterval(function() {
+                var defaultOpacity = element.css("opacity");
+                element
+                .animate({
+                    opacity: 1
+                }, 1000)
+                .animate({
+                    opacity: .3
+                }, 1000)
+                .animate({
+                    opacity: defaultOpacity
+                });
+        }, t), t);
+        t += 500;
+    });
 });
